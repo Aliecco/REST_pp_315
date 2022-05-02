039799861cda5bb0c9949bd083aa472e2e094606
Alisher Rakhymzhan <79269942+Aliecco@users.noreply.github.com>
2022-05-03 00:25:41 +0300

    Add files via upload

---
+++ src/test/java/com/rest_pp_315/RestPp315ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest_pp_315;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPp315ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
